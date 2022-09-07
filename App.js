8f5a85db4bd25cbc1c7f7a2b9cc540b66736f9ed
asteriskzuo <zuoyuhsywn@hotmail.com>
2022-09-07 17:56:59 +0800

    add react-native-chat-sdk is ok

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { ChatClient, ChatOptions } from "react-native-chat-sdk";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="haha"
+        onPress={() => {
+          ChatClient.getInstance().init(
+            new ChatOptions({ appKey: "", autoLogin: false })
+          );
+        }}
+      ></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
